715c3805455632443e67dfd2572de9e2ff4a7029
singhdivij <134772602+singhdivij@users.noreply.github.com>
2023-09-22 23:06:24 +0530

    Add files via upload

---
+++ DiceRoll.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Dice
+    {
+        public static void roll()
+        {
+            int playerRandomNumber, aiRandomNumber, playerCount = 0, aiCount = 0;
+
+            Console.WriteLine("                  WELCOME TO THE DICE GAME");
+            Console.WriteLine("\n-----------THIS WILL BE A BEST OUT OF THREE GAME-----------");
+            Console.WriteLine("\n>>> YOU ARE PLAYER 1.");
+            
+            Random random = new Random();
+
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine("Press enter key to roll the dice...");
+                Console.ReadLine();
+
+                playerRandomNumber = random.Next(1, 7);
+                Console.WriteLine($"You rolled a: {playerRandomNumber}");
+
+                Console.WriteLine("...");
+
+                System.Threading.Thread.Sleep(2000);
+
+                aiRandomNumber = random.Next(1, 7);
+                Console.WriteLine($"AI rolled a: {aiRandomNumber}");
+                
+                if(playerRandomNumber > aiRandomNumber)
+                {
+                    playerCount++;
+                    Console.WriteLine($"Your Score: {playerCount}   AI Score: {aiCount}");
+                }
+                else if (playerRandomNumber < aiRandomNumber)
+                {
+                    aiCount++;
+                    Console.WriteLine($"Your Score: {playerCount}   AI Score: {aiCount}");
+                }
+                else
+                {
+                    Console.WriteLine("Draw point!! No score given to the players.");
+                    Console.WriteLine($"Your Score: {playerCount}   AI Score: {aiCount}");
+                }
+                Console.WriteLine($"\nFinal scores are: Player: {playerCount} and AI: {aiCount}");
+            }
+            if(playerCount > aiCount)
+            {
+                Console.Write("Therefore, You Win.");
+            }
+            else if (playerCount < aiCount)
+            {
+                Console.Write("Therefore, AI Wins.");
+            }
+            else 
+            { 
+                Console.Write("Hence, Match Draw!"); 
+            }
+            Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+            roll();
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ NumberGuessing.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NumberGuessing
+{
+    internal class NumberGuessing
+    {
+        public static void numGuess()
+        {
+            int choice;
+            Console.WriteLine("            WELCOME TO THE NUMBER GUESSING GAME");
+            Console.WriteLine("CHOOSE A NUMBER BETWEEN 1 TO 10...");
+            while (true)
+            {
+                Console.Write("ENTER YOUR NUMBER: ");
+
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    if (choice >= 1 && choice <= 10)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 10.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input! Enter an integer between 1 and 10.");
+                }
+            }
+
+            Random random = new Random();
+
+            int checkNum = random.Next(1, 11);
+            Console.WriteLine($"Generated number is {checkNum}");
+
+            if(choice == checkNum)
+            {
+                Console.WriteLine("Congratulations! You guessed correctly.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            numGuess();
+            string yorn;
+
+            while (true)
+            {
+                Console.Write("Do you want to run the program? (Y/N): ");
+                yorn = Console.ReadLine();
+
+                if (yorn.Equals("N", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Exiting the program.");
+                    break; // Exit the loop when the user enters 'N' (case-insensitive)
+                }
+                else if (yorn.Equals("Y", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Put your program logic here
+                    numGuess();
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter 'Y' or 'N'.");
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
