edec5247cc92831d87700960423128024969b502
singhdivij <134772602+singhdivij@users.noreply.github.com>
2023-10-13 01:21:07 +0530

    Checks for all the prime numbers in a given range.

---
+++ Primes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primes
+{
+    internal class Primes
+    {
+        static void Main(string[] args)
+        {
+            List<int> primes = new List<int>();
+
+            Console.WriteLine("ENTER A RANGE OF VALUES: ");
+            int range = Convert.ToInt32(Console.ReadLine());
+
+            for (int number = 2; number <= range; number++)
+            {
+                List<int> divisorCount = new List<int>();
+
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        divisorCount.Add(i);
+                    }
+                }
+
+                if (divisorCount.Count == 2 && divisorCount[0] == 1 && divisorCount[1] == number)
+                {
+                    primes.Add(number);
+                }
+
+            }
+            foreach (int i in primes)
+            {
+                Console.Write(i + " ");
+            }
+            Console.Write($"\nare the prime numbers upto {range}.");
+            Console.ReadLine();
+        }
+    }
+}
